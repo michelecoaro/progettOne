6fb8b35024c87aa93c4c2342b41759c9ca8a3d44
Michele Coaro <michele.coaro@studenti.unimi.it>
2023-10-15 10:03:25 +0200

    Uploading Python Project (ProgettOne)

---
+++ progettOne/progettone/functions.py
@@ -0,0 +1,29 @@
+def create_deck():
+    deck=[]
+    seeds= 'S C H D'.split()
+    values=[str(x) for x in range (2,11)]+['A','J','Q','K']
+    for value in values:
+        for seed in seeds:
+            deck.append((value,seed))
+    return deck
+
+def create_simple_deck():
+    deck=[]
+    seeds= 'S C H D'.split()
+    values=[str(x) for x in range (2,8)]+['A','J','Q','K']
+    for value in values:
+        for seed in seeds:
+            deck.append((value,seed))
+    return deck
+
+
+def create_decks(number:int = 1, deck_generator=create_deck):
+    decks = []
+    seeds = 'S C H D'.split()
+    values = [str(x) for x in range (2,11)]+['A','J','Q','K']
+    for i in range (number):
+        deck = deck_generator()
+        decks.extend(deck)
+    return decks
+
+create_decks(4, deck_generator= create_deck)
---
+++ progettOne/tests/testOne.py
@@ -0,0 +1,7 @@
+from progettone import functions as f
+
+n,t = 4,f.create_simple_deck
+
+d1 = f.create_decks(number = n,deck_generator = t)
+
+print(d1)
